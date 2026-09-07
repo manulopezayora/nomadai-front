cf1085a2fe5099e9989b169dc68215c5b956a199
Manu <manulopezayora1985@protonmail.com>
2026-09-07 19:22:15 +0200

    feat(profile): add avatar section

--- src/i18n/locale/es/index.ts
+++ src/i18n/locale/es/index.ts
@@ -2,5 +2,6 @@ import api from './api.json';
 import auth from './auth.json';
 import common from './common.json';
 import header from './header.json';
+import user from './user.json';
 
-export default { common, auth, api, header };
+export default { common, auth, api, header, user };
--- src/modules/users/views/UserProfileView.vue
+++ src/modules/users/views/UserProfileView.vue
@@ -1,9 +1,41 @@
+<script setup lang="ts">
+import UserAvatar from '@/modules/shared/components/UserAvatar.vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
+const openPhotoUploader = () => {
+  // Lógica para abrir selector de archivo o modal
+};
+</script>
 <template>
-  <div>
-    <h1>User Profile</h1>
+  <div class="profile">
+    <div class="profile-header">
+      <UserAvatar :size="140" editable :show-dropdown="false" @change-photo="openPhotoUploader" />
+    </div>
+    <h2 class="profile-title">{{ t('user.editProfile') }}</h2>
+    <h3 class="profile-subtitle">{{ t('user.editProfileDescription') }}</h3>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<style scoped>
+.profile {
+  padding: 20px;
+  text-align: center;
+}
 
-<style scoped></style>
+.profile-header {
+  display: flex;
+  justify-content: center;
+}
+
+.profile-title {
+  margin-top: 25px;
+  font-weight: 600;
+  font-size: 1.5rem;
+}
+
+.profile-subtitle {
+  font-size: 1.2rem;
+}
+</style>
