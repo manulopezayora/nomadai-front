56ce6f7dfb14a0cdcef2474a4a3c64406bd1b5ea
Manu <manulopezayora1985@protonmail.com>
2026-09-10 17:19:20 +0200

    fix(main): show header and footer correctly

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
 import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
-import { RouterView } from 'vue-router';
+import { RouterView, useRoute } from 'vue-router';
 import { Toaster } from 'vue-sonner';
 import { useAuthStore } from './modules/auth/stores/auth.store';
 import FooterComponent from './modules/shared/components/FooterComponent.vue';
 import HeaderMenuComponent from './modules/shared/components/HeaderMenuComponent.vue';
 
+import { computed } from 'vue';
 import 'vue-sonner/style.css';
 
 const authStore = useAuthStore();
+const route = useRoute();
+const showComponents = computed(
+  () => authStore.isAuthenticated && !route.fullPath.includes('/auth'),
+);
 </script>
 
 <template>
   <Toaster richColors />
-  <HeaderMenuComponent v-if="authStore.isAuthenticated" />
+  <HeaderMenuComponent v-if="showComponents" />
   <RouterView />
-  <FooterComponent v-if="authStore.isAuthenticated" />
+  <FooterComponent v-if="showComponents" />
   <VueQueryDevtools />
 </template>
 
